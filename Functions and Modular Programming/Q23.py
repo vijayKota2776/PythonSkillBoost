aa229dc9f8a7ce2dc300e45cd87df85190abbd35
vijay kota <VijayKota2776@gmail.com>
2025-01-12 00:53:28 +0530

    Update Q23.py

---
+++ Functions and Modular Programming/Q23.py
@@ -0,0 +1,5 @@
+def count_char(string):
+    return len(string)
+input_string = input("Enter a string: ")
+char_count = count_char(input_string)
+print(f"The number of characters in the string '{input_string}' is: {char_count}")
